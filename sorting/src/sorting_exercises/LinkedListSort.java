c3b91fa787bc992ff34f19776aa14e8e1fea38d7
edugreat <anyanwuchinedu687@yahoo.com>
2023-08-19 07:29:14 +0100

    implements linked list sorting & shuffling algorithm

---
+++ sorting/src/sorting_exercises/LinkedListSort.java
@@ -0,0 +1,62 @@
+package sorting_exercises;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * Implement a natural mergesort for linked lists.
+ */
+public class LinkedListSort {
+
+	public static void main(String[] args) {
+	
+	}
+	
+	
+ public  static <T extends Comparable<T>>  void mergeSort(List<T> t) {
+	 
+	 
+	 
+	 if(t.size() < 2)
+		 return;
+	 
+		 int hi = t.size();
+		 int middle = hi/2;
+		 
+	 List<T> leftHalf = new ArrayList<>(t.subList(0, middle));
+	 
+	 
+	  
+	  List<T> rightHalf = new ArrayList<>(t.subList(middle, hi));
+	  
+	  mergeSort(leftHalf);
+	  mergeSort(rightHalf);
+	  merge(t, leftHalf, rightHalf);
+	 
+ }
+ 
+ private static <T extends Comparable<T>> void merge(List<T> orignal, List<T> leftHalf, List<T> rightHalf) {
+	
+	 int x = 0;//for leftHalf list
+	 int y = 0;//for rightHalf list
+	 int z = 0;//for the list to which we intend to merge both left and right
+	
+	 while(x < leftHalf.size() && y < rightHalf.size()) {
+		 if(lessOrEquals(leftHalf.get(x), rightHalf.get(y)))
+			 orignal.set(z++, leftHalf.get(x++));
+		 else
+			 orignal.set(z++, rightHalf.get(y++));
+	 
+	 }
+	 while(x < leftHalf.size())
+		 orignal.set(z++, leftHalf.get(x++));
+	 
+	 while(y < rightHalf.size())
+		 orignal.set(z++, rightHalf.get(y++));
+ }
+ 
+ private static <T extends Comparable<T>> boolean lessOrEquals(T a, T b) {
+	 
+	 return (a.compareTo(b) < 0) || (a.compareTo(b) == 0);
+ }
+}
---
+++ sorting/src/sorting_exercises/ShufflingList.java
@@ -0,0 +1,72 @@
+package sorting_exercises;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+/*
+ * Shuffling a list.Develop and implement a divide-and-conquer algorithm that randomly shuffles
+ * a linked list in linearithmic time and logarithmic extra space
+ */
+public class ShufflingList {
+
+	public static void main(String[] args) {
+		
+	}
+
+	public static <T extends Comparable<T>> void shuffle(List<T> list) {
+		
+		//initial terminating check
+		if(list.size() < 2) return;
+		
+		int size = list.size();
+		int middle = size/2;
+		
+		List<T> leftHalf = new ArrayList<>(list.subList(0, middle));
+		List<T> rightHalf = new ArrayList<>(list.subList(middle, size));
+		
+		//recursive calls
+		shuffle(leftHalf);
+		shuffle(rightHalf);
+		shuffling(list, leftHalf, rightHalf);
+		
+		
+	}
+	
+	private static <T extends Comparable<T>> void shuffling(List<T> list, List<T> leftHalf, List<T> rightHalf) {
+		
+		int listSize = list.size();
+		//get some unique random numbers. Each of the random numbers must be less than the size of the list
+		Random rand = new Random();
+		Integer[] randomArr = new Integer[listSize];
+		
+		for(int i = 0; i < listSize;) {
+			int r = rand.nextInt(listSize);
+			if(!contains(randomArr, r))
+				randomArr[i++] = r;
+		}
+		
+		int x = 0, y = 0, z =0;
+		
+		while(true) {
+			if(x < leftHalf.size())
+				list.set(randomArr[z++], leftHalf.get(x++));
+			
+			if(y < rightHalf.size())
+				list.set(randomArr[z++], rightHalf.get(y++));
+			
+			if(x >= leftHalf.size() && y >= rightHalf.size()) break;
+			
+		}
+		
+	}
+	
+	private static boolean contains(Integer[]a, Integer i) {
+
+		for(Integer x: a)
+			if(x == i)
+				return true;
+		
+		return false;
+	}
+}
